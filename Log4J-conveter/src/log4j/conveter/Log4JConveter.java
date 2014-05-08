7158bbe2faa2236ce5dec25476c590c883c3560a
vladimircaniga@gmail.com <vladimircaniga@gmail.com@5f3a8a08-852c-d6d8-ab7a-810353872824>
2014-05-08 22:29:21 +0000

    Added DTD validation

--- Log4J-conveter/src/log4j/conveter/Log4JConveter.java
+++ Log4J-conveter/src/log4j/conveter/Log4JConveter.java
@@ -4,16 +4,45 @@
  */
 package log4j.conveter;
 
+import java.io.IOException;
+
 /**
  *
  * @author Michal
  */
 public class Log4JConveter {
 
+    private static String getFileExtension(String fileName) {
+        String[] strings = fileName.split("\\.");
+        if (strings.length <= 1) {
+            System.err.println("File argument must end with .xml or .properties");
+            System.exit(1);
+        }
+
+        return strings[strings.length - 1];
+    }
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Toto raz bude fungujuci program!");
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.err.println("No input file specified by argument.");
+            System.exit(4);
+        }
+        String fileExtension = getFileExtension(args[0]);
+        switch (fileExtension) {
+            case "xml":
+                XMLToProperties xmlToProperties = new XMLToProperties(args[0]);
+                xmlToProperties.validate();
+                break;
+            case "properties":
+                PropertiesToXML propertiesToXml = new PropertiesToXML(args[0]);
+                break;
+            default:
+                System.err.println("File name must end with .xml or .properties");
+                System.exit(2);
+        }
+
     }
 }
